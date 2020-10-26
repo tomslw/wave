9144636b40a5970bdcc9a97da558aec3ba11b86e
Toms Strals <stralstomslw@gmail.com>
2020-10-26 14:04:05 +0200

    configured it to change the brightness of the led

--- wave.c
+++ wave.c
@@ -1,13 +1,18 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#include <util/delay.h>
+//#include <util/delay.h>
 #include "adc.h"
-#include "uart.h"
+//#include "uart.h"
 
 ISR(ADC_vect)
 {
-  // sets the new TOP value for the timer from analog
+  // sets the new TOP value for the timer from analog (on differant mode)
+  //OCR1A = ADC;
+
+
+  // set the new compare value
   OCR1A = ADC;
+
   //printf("adc interrupt \n");
   //ADCSRA &= ~(1 << ADSC);
 
@@ -30,23 +35,26 @@ int main(){
 
   sei(); // enable interrups
   adc_init();
-  uart_init();
-  stdout = &uart_output;
+  //uart_init();
+  //stdout = &uart_output;
 
 
 
   //TCCR1B |= (1 << CS12) | (1 << CS10); // enables timer and sets the prescaler - 1024
   //TCCR1B |= (1 << CS12); // enables timer and sets the prescaler - 256
   //TCCR1B |= (1 << CS11) | (1 << CS10); // enables timer and sets the prescaler - 64
-  TCCR1B |= (1 << CS11); // enables timer and sets the prescaler - 8
-  //TCCR1B |= (1 << CS10); // to 1
+  //TCCR1B |= (1 << CS11); // enables timer and sets the prescaler - 8
+  TCCR1B |= (1 << CS10); // to 1
 
   //TIMSK1 |= (1 << TOIE1);  // enables interrupt on each timer tick to update the TOP value
 
-  TCCR1A |= (1 << WGM10) | (1 << WGM11);
-  TCCR1B |= (1 << WGM13) | (1 << WGM12);  // sets up the Waveform Generation Mode to the nr15
+  //TCCR1A |= (1 << WGM10) | (1 << WGM11);
+  //TCCR1B |= (1 << WGM13) | (1 << WGM12);  // sets up the Waveform Generation Mode to the nr15
 
+  TCCR1A |= (1 << WGM11);
+  TCCR1B |= (1 << WGM12)|(1 << WGM13);  // fast PWM mode that looks at ICR1 for TOP
 
+  ICR1 = 0xFFFF; // max
 
   // ADC =====================
   ADCSRB |= (1 << ADTS2) | (1 << ADTS1);  // configures the adc autotrigger to timer1 overflow
@@ -56,7 +64,7 @@ int main(){
 
   ADMUX &= 0xf0; // clears the selected pin
   ADMUX |= (uint8_t)0;   // selects the pin
-  ADMUX |= (1 << ADLAR);
+  ADMUX |= (1 << ADLAR); // Left aligned the top value register for more optimized functionality (apart of the float removal)
   // =========================
 
 
@@ -65,7 +73,7 @@ int main(){
 
   //PORTB ^= (1 << DDB5);
 
-  TCCR1A |= (1 << COM1A0); // makes it toggle the 9 port every time there is a compare
+  TCCR1A |= (1 << COM1A0) | (1 << COM1A1); // make it turn on the pin on compare
 
   while(1) {}
 
