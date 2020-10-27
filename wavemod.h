d97c5f831e5f57c04adb8513f28cdf483b28a72b
Toms Strals <stralstomslw@gmail.com>
2020-10-27 10:48:52 +0200

    comments added for clearity

--- wavemod.h
+++ wavemod.h
@@ -1,7 +1,7 @@
 #ifndef _WAVEMOD_H
 #define _WAVEMOD_H
 
-extern int * const timer_value;
+extern int * const timer_value; // can be from 0x0000 to 0xFFFF
 
 enum PrescalerValues{
     V1 = 1,
@@ -11,9 +11,9 @@ enum PrescalerValues{
     V1024 = 1024
 };
 
-void TopSwitch();
+void TopSwitch();   // works only on the DDB1 port
 
-void CompareSwitch(int pin); // have to set the prescaler before this btw (only DDB1 and DDB2 pins are valid)
+void CompareSwitch(int pin);    // have to set the prescaler before this btw (only DDB1 and DDB2 pins are valid)
 
 void SetPrescaler(enum PrescalerValues val);
 
